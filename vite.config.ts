b40806d95f69b202a94a5a8f2769a83a5f092250
Alessandro <raydalessandro@github.com>
2025-11-24 17:16:10 +0100

    Fix: riscrive vite.config.ts per correggere errore Vercel

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+});
